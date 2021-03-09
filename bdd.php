2ac64060c3237963fae6f4fc067254422ebdae44
William Mendoza <Ing_William_Mendoza@outlook.com>
2021-03-09 10:13:35 -0500

    correciones al codigo CRUD

--- bdd.php
+++ bdd.php
@@ -1,9 +1,6 @@
 <?php
-try 
-{
-        $bbd = new PDO('mysql:host=localhost;port=33065;dbname=swchamezadb;charset=utf8', 'root', 'toor');
-} 
-catch (Exception $e) 
-{
+try {
+        $bdd = new PDO('mysql:host=localhost;port=33065;dbname=swchamezadb;charset=utf8', 'root', 'toor');
+} catch (Exception $e) {
         die('Error : ' . $e->getMessage());
 }
